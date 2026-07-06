c803b397712f45ac042c577a9633d59b10d4dd87
ShivamRaj0777 <shivamgaming920966453@gmail.com>
2026-07-06 19:11:41 +0530

    Create password_strength_checker.py

---
+++ password_strength_checker.py
@@ -0,0 +1,22 @@
+import re
+
+password = input("Enter Password: ")
+score = 0
+
+if len(password) >= 8:
+    score += 1
+if re.search(r"[A-Z]", password):
+    score += 1
+if re.search(r"[a-z]", password):
+    score += 1
+if re.search(r"[0-9]", password):
+    score += 1
+if re.search(r"[@$!%*?&]", password):
+    score += 1
+
+if score == 5:
+    print("Strong Password")
+elif score >= 3:
+    print("Medium Password")
+else:
+    print("Weak Password")
